e659c4cb66f873ed62d438f94d80a4f783ffffc2
Cosmin Grozea <cosmin.grozea94@gmail.com>
2016-09-30 11:03:35 +0300

    Ref #6 Add test case for trac ticket nr 29052

--- tests/acceptance/C03DomainListCest.php
+++ tests/acceptance/C03DomainListCest.php
@@ -4,9 +4,11 @@ namespace Plesk;
 
 use WebGuy;
 use Page\DomainListPage;
+use Page\ConfigurationPage;
 use Page\ProfessionalSpamFilterPage;
 use Step\Acceptance\CommonSteps;
 use Step\Acceptance\DomainListSteps;
+use Codeception\Util\Locator;
 
 class C03DomainListCest
 {
@@ -17,6 +19,7 @@ class C03DomainListCest
      */
     public function _before(DomainListSteps $I)
     {
+        // Login as root
         $I->loginAsRoot();
     }
 
@@ -25,7 +28,7 @@ class C03DomainListCest
      */
     public function _after(DomainListSteps $I)
     {
-        $I->cleanupPlesk();
+        // $I->cleanupPlesk();
     }
 
     /**
@@ -33,7 +36,7 @@ class C03DomainListCest
      */
     public function _failed(DomainListSteps $I)
     {
-        $this->_after($I);
+        // $this->_after($I);
     }
 
     public function verifyDomainListAsRoot(DomainListSteps $I)
@@ -106,5 +109,55 @@ class C03DomainListCest
 
         // Check login functionality for the customer domain
         $I->checkLoginFunctionality($domain, false);
+    }
+
+    public function verifyLoginAsReseller(DomainListSteps $I)
+    {
+        // Go to addon "Configuration" page
+        $I->goToPage(ProfessionalSpamFilterPage::CONFIGURATION_BTN, ConfigurationPage::TITLE);
+
+        // Set configuration options needed for the test
+        $I->setConfigurationOptions(array(
+            Locator::combine(ConfigurationPage::ADD_ADDON_AS_ALIAS_PLESK_OPT_CSS, ConfigurationPage::ADD_ADDON_AS_ALIAS_PLESK_OPT_XPATH) => false
+        ));
+
+        // Create a new reseller account
+        list($resellerUsername, $resellerPassword, $resellerId) = $I->createReseller();
+
+        // Enable a shared IP
+        $I->shareIp($resellerId);
+
+        // Logout from root account
+        $I->logout();
+
+        // Login with the reseller account
+        $I->login($resellerUsername, $resellerPassword);
+
+        // Create customer
+        list($customerUsername, $customerPassword, $domain) = $I->createCustomer();
+
+        // Logout from reseller account
+        $I->logout();
+
+        // Login with the customer account
+        $I->loginAsClient($customerUsername, $customerPassword);
+
+        // Create alias
+        $alias = $I->addAliasAsClient($domain);
+
+        // Check that aliasdomain exists in Spampanel
+        $I->apiCheckDomainExists($alias);
+
+        // Logout from client account
+        $I->logout();
+
+        // Login with reseller account
+        $I->login($resellerUsername, $resellerPassword);
+
+        // Check if alias domain is present in list
+        $I->checkDomainList($alias);
+
+         // Check login functionality for the reseller domain
+        $I->checkLoginFunctionality($alias);
     }
 }
